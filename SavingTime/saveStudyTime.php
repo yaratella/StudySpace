903e920ff08874065c824cfdc1d81606890b2be2
Yara Zrigan <C836987719@cairo.cs.colostate.edu>
2026-04-16 19:57:12 -0600

    Added the pseudocode for how I'm going to make sure that the time saves for each individual person and doesn't get lost!

---
+++ SavingTime/saveStudyTime.php
@@ -0,0 +1,18 @@
+<?php
+
+//start session
+
+//copy from other files where you figured out how to connect to your data base
+//don't forget to include password.php and all other necessary files
+
+//define userID (just like in studystats.php)
+//data you'd have to decode and get the contends from php://input
+//and also figure out the hours too
+
+
+//then this will update the study hours (then this will be called by all other files that will need to update the time)
+
+
+//then close the stmt and the conn to everything in this file (so that new data can come and go)
+
+?>
